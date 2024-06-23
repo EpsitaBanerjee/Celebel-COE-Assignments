804ac6f2b2c4626ea2a57e448e842790a95d40d7
Epsita Banerjee <147192457+EpsitaBanerjee@users.noreply.github.com>
2024-06-23 19:00:50 +0530

    Update 4.sql

--- Week 5/4.sql
+++ Week 5/4.sql
@@ -1,11 +1,48 @@
-Create schema HumanResources
-Go
- 
-Select * into SQLShackDemo.HumanResources.Department from 
-AdventureWorks2018.HumanResources.Department 
-Select * into SQLShackDemo.HumanResources.Employee from 
-AdventureWorks2018.HumanResources.Employee 
-Select * into SQLShackDemo.HumanResources.EmployeeDepartmentHistory from 
-AdventureWorks2018.HumanResources.EmployeeDepartmentHistory 
-Select * into SQLShackDemo.HumanResources.EmployeePayHistory from 
-AdventureWorks2018.HumanResources.EmployeePayHistory
+DECLARE @source_database NVARCHAR(50) = 'ource_database_name'
+DECLARE @target_database NVARCHAR(50) = 'target_database_name'
+
+
+DECLARE @tables TABLE (table_name NVARCHAR(50), column_names NVARCHAR(4000))
+
+
+INSERT INTO @tables
+VALUES
+    ('table1', 'column1, column2, column3'),
+    ('table2', 'columnA, columnB'),
+    ('table3', 'columnX, columnY, columnZ')
+
+DECLARE @table_name NVARCHAR(50)
+DECLARE @column_names NVARCHAR(4000)
+DECLARE @sql NVARCHAR(4000)
+
+
+DECLARE cur CURSOR FOR
+SELECT table_name, column_names
+FROM @tables
+
+OPEN cur
+
+FETCH NEXT FROM cur INTO @table_name, @column_names
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    
+    SET @sql = 'IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = ''' + @table_name + ''' AND database_id = DB_ID(''' + @target_database + '''))
+                BEGIN
+                    CREATE TABLE ' @target_database + '.dbo.' + @table_name + '' + @column_names + ')
+                END'
+
+    
+    EXEC sp_executesql @sql
+
+    SET @sql = 'INSERT INTO ' @target_database + '.dbo.' + @table_name + '' + @column_names + ')
+                SELECT ' @column_names + 'ROM ' @source_database + '.dbo.' + @table_name
+
+   
+    EXEC sp_executesql @sql
+
+    FETCH NEXT FROM cur INTO @table_name, @column_names
+END
+
+CLOSE cur
+DEALLOCATE cur
